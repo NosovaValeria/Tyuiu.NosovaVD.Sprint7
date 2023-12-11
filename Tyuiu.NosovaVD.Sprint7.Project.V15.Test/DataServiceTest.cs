994ee2211be4de69fc8cfebb4afbc911ca10dbf6
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-12-11 15:03:46 +0500

    PushOne

---
+++ Tyuiu.NosovaVD.Sprint7.Project.V15.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NosovaVD.Sprint7.Project.V15.Lib;
+namespace Tyuiu.NosovaVD.Sprint7.Project.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.NosovaVD.Sprint7.Project.V15/FormMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tyuiu.NosovaVD.Sprint7.Project.V15.Lib;
+namespace Tyuiu.NosovaVD.Sprint7.Project.V15
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
